1f7ca8135cbe909abb7c3f20a54b8cbadacd9d1c
Beelzebob6666 <6Beelzebob6@gmail.com>
2026-06-25 07:08:04 +0200

    translations - show old ref only when there is one

--- js/web/_languages/js/_languages.js
+++ js/web/_languages/js/_languages.js
@@ -178,10 +178,11 @@ let Translation = {
 			let missing = targetValue.trim() === '';
 			let updated = !Translation.targetData?.[key]?.r || (reference.s || reference) !== Translation.targetData?.[key]?.r;
 			targetValue = localData?.[key]?.s || localData?.[key] || targetValue;
+			let OldRef = HTML.escapeHtml(Translation.targetData?.[key]?.r || '');
 			return `<tr class="${missing ? 'missing' : ''} ${updated ? 'updated' : ''}">
 				<td>${key}</td>
 				<td title="Comparison Value: ${HTML.escapeHtml(comparisonValue)}">${reference.s||reference}</td>
-				<td ${updated ? `title="Old Reference: ${HTML.escapeHtml(Translation.targetData?.[key]?.r || '')}"` : ''}>${(updated && targetValue != "") ? `<b title="click to confirm translation as correct">✓ </b>` : ''}<span>${targetValue}</span></td>
+				<td ${(updated && !!OldRef) ? `title="Old Reference: ${OldRef}"` : ''}>${(updated && !!targetValue) ? `<b title="click to confirm translation as correct">✓ </b>` : ''}<span>${targetValue}</span></td>
 			</tr>`;
 		}).join('');
 		$('#TranslationTable tbody').html(rowsHtml);
